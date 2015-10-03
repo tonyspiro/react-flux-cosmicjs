16444acaa5ecb5125d494d285316cf062adaee98
Tony Spiro <tspiro@tonyspiro.com>
2015-10-03 13:03:10 -0500

    edit config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 ================================ */
 let config = {};
 config.bucket = {
-  slug: 'easy-flux-cosmicjs', // this is your Cosmic JS bucket slug.  Sign up for Cosmic JS (Free) and add a bucket: https://cosmicjs.com/
+  slug: 'react-flux-cosmicjs', // this is your Cosmic JS bucket slug.  Sign up for Cosmic JS (Free) and add a bucket: https://cosmicjs.com/
   read_key: '', // add read_key if added to your Cosmic JS bucket settings
   write_key: '' // add write_key if added to your Cosmic JS bucket settings
 };
